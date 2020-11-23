733a8ee5c1fa66cd3cc2086e159e4df90ee394e7
Damien Valentini <damien.valentini@laposte.net>
2020-11-23 17:06:08 +0100

    Insert_table_papyrus
    entcom et fournis

---
+++ HeidiMySQL/Papyrus/Création/fournis.sql
@@ -0,0 +1,7 @@
+INSERT INTO `fournis` (`numfou`, `nomfou`, `ruefou`, `posfou`, `vilfou`, `confou`, `satisf`) VALUES
+	(120, 'GROBRIGAN', '20 rue du papier', '92200', 'papercity', 'Georges', 8),
+	(540, 'ECLIPSE', '53 rue laisse flotter les rubans', '78250', 'Bugbugville', 'Nestor', 7),
+	(8700, 'MEDICIS', '120 rue des plantes', '75014', 'Paris', 'Lison', 0),
+	(9120, 'DISCOBOL', '11 rue des sports', '85100', 'La Roche sur Yon', 'Hercule', 8),
+	(9150, 'DEPANPAP', '26 avenue des locomotives', '59987', 'Coroncountry', 'Pollux', 5),
+	(9180, 'HURRYTAPE', '68 boulevard des octets', '40440', 'Dumpville', 'Track', 0);
