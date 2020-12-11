d6e6727aca6a79fc6abd4de7e86e27a26108e43c
Damv <damien.valentini@laposte.net>
2020-12-11 11:21:38 +0100

    Update delete_script.php

---
+++ PHP/jarditou_Php_PDO/delete_script.php
@@ -0,0 +1,20 @@
+<?php 
+
+$pro_id=$_GET['pro_id'];
+require "connexion_hotelDB.php";
+
+$requete = $db->prepare("DELETE FROM produits WHERE pro_id=:pro_id");
+
+$requete->bindValue(':pro_id', $pro_id, PDO::PARAM_INT);
+
+$requete->execute();
+
+// Libération de la connexion au serveur de BDD
+$requete->closeCursor();
+
+// Redirection vers index.php
+header("Location: tableau_Copie.php");
+exit;
+
+
+?>
