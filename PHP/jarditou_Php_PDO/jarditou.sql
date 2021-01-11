da9f6d014c58c45638b9a7f043c603aa621ccd2b
Damv <damien.valentini@laposte.net>
2021-01-11 08:44:25 +0100

    update

--- PHP/jarditou_Php_PDO/jarditou.sql
+++ PHP/jarditou_Php_PDO/jarditou.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `jarditou`
 --
+CREATE DATABASE IF NOT EXISTS jarditou;
+
+USE jarditou;
 
 -- --------------------------------------------------------
 
