efbdaebe0daa3fa006d161a9b3527aeab762e787
Damv <damien.valentini@laposte.net>
2021-01-11 15:56:30 +0100

    Update login_script.php

--- PHP/jarditou_Php_PDO/login_script.php
+++ PHP/jarditou_Php_PDO/login_script.php
@@ -13,6 +13,7 @@ date_default_timezone_set('Europe/Paris');
 $users_login=valid_donnees($_POST['login']);
 $users_motdepasse=valid_donnees($_POST['mdp']);
 $users_derniere_connexion=new DateTime();
+$tentative;
 
 function valid_donnees($donnees){
     $donnees = trim($donnees);
@@ -33,6 +34,7 @@ if (!empty($users_login)
     $requete->execute(array($users_login));
     $check=$requete->fetchAll();
     
+
     if(count($check)==0)
     {
         echo "Identifiant invalide";
@@ -44,29 +46,43 @@ if (!empty($users_login)
         $check2=$requete2->fetch(PDO::FETCH_ASSOC);
         $users_nom= ucfirst($check2["users_nom"]);
         $users_prenom= ucfirst($check2["users_prenom"]);
+        //$users_role= $check2["users_role"]; CREER LA COLONNE users_role en BDD
 
+        /*$requete4= "SELECT users_tentative FROM users WHERE users_login=$users_login"; CREER LA COLONNE TENTATIVE EN BDD
+        $result4= $db->query($requete4);
 
-        if (password_verify($users_motdepasse,$check2['users_motdepasse'])) {
-            echo "mot de passe valide";
-            //$_SESSION["login"]=$users_login;
-            $_SESSION["login"]=$users_login;
-            $_SESSION["users_nom"]=$users_nom;
-            $_SESSION["users_prenom"]=$users_prenom;
-
-            $requete3 = $db->prepare("UPDATE users SET users_derniere_connexion=:users_derniere_connexion WHERE users_login=:users_login");
-            $requete3->bindValue(':users_derniere_connexion', $users_derniere_connexion->format('Y-m-d H:i:s'));
-            $requete3->execute($users_derniere_connexion);
-            $requete3->closeCursor(); //probleme ne stock pas en bdd
-
-            $_SESSION["users_derniere_connexion"]=$users_derniere_connexion;
-
-            header("location: index.php");
-            exit();
+        if($tentative>=3)
+        {
+            echo "trop de tentative, veuillez revenir plus tard"
+            header("Location: login_form.php")
         }
         else
-        {
-            echo "mot de passe invalide";
-        }
+        {*/
+            if (password_verify($users_motdepasse,$check2['users_motdepasse'])) 
+            {
+                echo "mot de passe valide";
+                //$_SESSION["login"]=$users_login;
+                $_SESSION["login"]=$users_login;
+                $_SESSION["users_nom"]=$users_nom;
+                $_SESSION["users_prenom"]=$users_prenom;
+
+                $requete3 = $db->prepare("UPDATE users SET users_derniere_connexion=:users_derniere_connexion WHERE users_login=:users_login");
+                $requete3->bindValue(':users_derniere_connexion', $users_derniere_connexion->format('Y-m-d H:i:s'));
+                $requete3->execute($users_derniere_connexion);
+                $requete3->closeCursor(); //probleme ne stock pas en bdd edit: devrait être regler avec insert to de la date à l'enregistrement
+
+                $_SESSION["users_derniere_connexion"]=$users_derniere_connexion;
+                //$_SESSION["role"]=$users_role;
+
+                header("location: index.php");
+                exit();
+            }
+            else
+            {
+                echo "mot de passe invalide";
+                //$tentative++
+            }
+        //}
     }
     
     
