07630c661d09155728fc34a1be49139b0c4a1c18
Damien Valentini <damien.valentini@laposte.net>
2021-01-13 16:24:11 +0100

    create_poo

---
+++ POO/tests/Personnage.class.php
@@ -0,0 +1,15 @@
+class Personnage
+{
+    public $_nom;
+    public $_prenom;
+    public $_age;
+    public $_sexe;
+
+    function__construct($nom,$prenom,$age,$sexe)
+    {
+        $this->_nom=nom;
+        $this->_prenom=prenom;
+        $this->_age=age;
+        $this->_sexe=sexe;
+    }
+}
