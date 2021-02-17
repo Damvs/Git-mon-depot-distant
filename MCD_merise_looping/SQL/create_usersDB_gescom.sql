de7ee4a4913d9445b19a20368c41fd073889f404
Damv <damien.valentini@laposte.net>
2021-02-17 09:43:37 +0100

    Update create_usersDB_gescom.sql

--- MCD_merise_looping/SQL/create_usersDB_gescom.sql
+++ MCD_merise_looping/SQL/create_usersDB_gescom.sql
@@ -2,14 +2,14 @@ CREATE USER IF NOT EXISTS "util1"@"localhost" IDENTIFIED BY "mdp";
 CREATE USER IF NOT EXISTS "util2"@"localhost" IDENTIFIED BY "mdp";
 CREATE USER IF NOT EXISTS "util3"@"localhost" IDENTIFIED BY "mdp";
 
-GRANT ALL
+GRANT ALL PRIVILEGES
 ON gescom_afpa.*
-TO util1
+TO "util1"@"localhost";
 
 GRANT SELECT
 ON gescom_afpa.*
-TO util2
+TO "util2"@"localhost";
 
 GRANT SELECT
 ON gescom_afpa.suppliers
-TO util3
+TO "util3"@"localhost";
