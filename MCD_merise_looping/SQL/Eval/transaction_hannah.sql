33753d709c9455f2410f0301cbef56450be74a73
Damien Valentini <damien.valentini@laposte.net>
2021-03-23 18:00:56 +0100

    Update transaction_hannah.sql

--- MCD_merise_looping/SQL/Eval/transaction_hannah.sql
+++ MCD_merise_looping/SQL/Eval/transaction_hannah.sql
@@ -1,14 +1,12 @@
+INSERT INTO posts (pos_libelle) VALUES ("Retraité")
 
+START TRANSACTION;
+SET @posid = (SELECT emp_pos_id FROM employees WHERE emp_lastname LIKE 'HANNAH' and emp_firstname LIKE "Amity");
 UPDATE 
     employees 
 SET 
-    emp_pos_id = (
-            SELECT 
-                emp_pos_id 
-            FROM 
-                employees 
-            WHERE 
-                emp_lastname LIKE 'HANNAH' and emp_firstname LIKE "Amity"), emp_salary = emp_salary*1.05 
+    emp_pos_id = @posid, 
+    emp_salary = emp_salary*1.05 
 WHERE emp_id = (
             SELECT 
                 emp_id  
@@ -18,25 +16,32 @@ WHERE emp_id = (
                 posts ON emp_pos_id = pos_id 
             WHERE pos_libelle LIKE "Pépiniériste" ORDER BY `emp_enter_date` ASC LIMIT 1);
 
-UPDATE
-    employees
-SET
-    emp_pos_id =(
-        SELECT
-            emp_pos_id
-        FROM
-            employees
-        WHERE
-            emp_lastname LIKE 'HANNAH' AND emp_firstname LIKE "Amity")
-    , emp_salary = emp_salary * 1.05
-WHERE
-    emp_id =(
-    SELECT
-        emp_id
-    FROM
-        `employees`
-    JOIN posts ON `emp_pos_id` = pos_id
-    WHERE
-        pos_libelle LIKE "pépiniériste"
-    ORDER BY
-        `emp_enter_date` ASC LIMIT 1);
+UPDATE 
+    employees as emp
+SET 
+    emp_pos_id = (SELECT pos_id FROM posts WHERE pos_libelle LIKE "Retraité") 
+WHERE emp_pos_id = @posid;
+
+
+-- UPDATE
+--     employees
+--     SET
+--     emp_pos_id =(
+--         SELECT
+--             emp_pos_id
+--         FROM
+--             employees
+--         WHERE
+--             emp_lastname LIKE 'HANNAH' AND emp_firstname LIKE "Amity")
+--     , emp_salary = emp_salary * 1.05
+--     WHERE
+--     emp_id =(
+--     SELECT
+--         emp_id
+--     FROM
+--         `employees`
+--     JOIN posts ON `emp_pos_id` = pos_id
+--     WHERE
+--         pos_libelle LIKE "pépiniériste"
+--     ORDER BY
+--         `emp_enter_date` ASC LIMIT 1);
